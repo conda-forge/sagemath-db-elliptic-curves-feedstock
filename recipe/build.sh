0ade1a159732410874a4575fc9d3de2f809f9e7a
Isuru Fernando <isuruf@gmail.com>
2019-06-20 07:32:05 +0200

    Update to 0.8.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,5 @@
 cd elliptic_curves-$PKG_VERSION
 export SAGE_SHARE="$PREFIX/share"
 ln -s . src
-curl -L -O https://raw.githubusercontent.com/sagemath/sage/8.4/build/pkgs/elliptic_curves/spkg-install.py
+curl -L -O https://raw.githubusercontent.com/sagemath/sage/8.8.rc1/build/pkgs/elliptic_curves/spkg-install.py
 python spkg-install.py
