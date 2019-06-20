50f57bff4f8fe093c9fef0a2074604b222045045
Isuru Fernando <isuruf@gmail.com>
2019-06-20 07:47:19 +0200

    Fix build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd elliptic_curves-$PKG_VERSION
 export SAGE_SHARE="$PREFIX/share"
 ln -s . src
 curl -L -O https://raw.githubusercontent.com/sagemath/sage/8.8.rc1/build/pkgs/elliptic_curves/spkg-install.py
